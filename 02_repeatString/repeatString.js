5bdac297c3715d84be5dc0824e1124b7a5a3047b
Hamza Amar <rhamzax@gmail.com>
2022-06-18 19:57:23 -0600

    Complete repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, amount) {
+    if (amount < 0){
+        return "ERROR";
+    }
+    let phrase = "";
+    for (let i = 0; i < amount; i++){
+        phrase += word;
+    }
+    return phrase;
 };
 
 // Do not edit below this line
