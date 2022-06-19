05a70f687c51b7ac9d03a5170da14449c76c5362
Hamza Amar <rhamzax@gmail.com>
2022-06-18 20:47:25 -0600

    Complete removing an element from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
+    let array = arguments[0];
 
+    for (let i = 0; i < arguments.length - 1; i++){
+        let index = array.indexOf(arguments[i+1])
+        if (index >= 0){
+            array.splice(index, 1)
+        }
+        
+    }
+    return array;
 };
 
 // Do not edit below this line
