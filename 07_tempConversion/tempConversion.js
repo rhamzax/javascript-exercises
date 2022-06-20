028f578580e18bb12f20faee79604b0b49c73298
Hamza Amar <rhamzax@gmail.com>
2022-06-20 00:41:25 -0600

    Solve tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let celcius = (fahrenheit - 32) * 5 / 9
+  if (Number.isInteger(celcius)){
+    return celcius;
+  }
+  else{
+    return (celcius.toFixed(1) * 1);
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  let fahrenheit = (celcius * 9 / 5)+ 32
+  if (Number.isInteger(fahrenheit)){
+    return fahrenheit;
+  }
+  else{
+    return (fahrenheit.toFixed(1)* 1);
+  }
 };
 
 // Do not edit below this line
